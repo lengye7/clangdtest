2e249ed5c34c7db81a68b6149833d7ceb5e569d1
lengye7 <zhangzhuo_ming@outlook.com>
2025-02-12 16:01:51 +0800

    add clang-format code

---
+++ clang-format/src/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+#include <iostream>
+
+void hello(std::string str){
+    std::cout<<"hello"<<str<<std::endl;
+}
---
+++ clang-format/src/hello.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_H
+#define HELLO_H
+#include <string>
+void hello(std::string str);
+
+#endif
